f8e6b1e087ce89f13ca7569f69cfca73be7ab39d
李文 <lwen7@grgbanking.com>
2020-02-28 11:24:54 +0800

    多语言设置

--- src/main.js
+++ src/main.js
@@ -9,8 +9,29 @@ import 'normalize.css/normalize.css'
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import zhLocale from 'element-ui/lib/locale/lang/zh-CN'
+import enLocale from 'element-ui/lib/locale/lang/en'
 
-Vue.use(ElementUI)
+import VueI18n from 'vue-i18n'
+
+import { Flexbox, FlexboxItem } from '@/components/flexbox'
+
+Vue.use(VueI18n)
+Vue.use(ElementUI, {
+  i18n: (key, value) => i18n.t(key, value)
+})
+
+Vue.component('flexbox', Flexbox)
+Vue.component('flexbox-item', FlexboxItem)
+
+// 多语言
+const i18n = new VueI18n({
+  locale: 'zh',
+  messages: {
+    zh: Object.assign(require('@/common/lang/zh'), zhLocale),
+    en: Object.assign(require('@/common/lang/en'), enLocale)
+  }
+})
 
 Vue.config.productionTip = false
 
