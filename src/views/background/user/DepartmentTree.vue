395a90c9282f3b22551c358eced8eb9c7f417ddc
李文 <lwen7@grgbanking.com>
2020-03-27 10:17:28 +0800

    部门树设置单选

--- src/views/background/user/DepartmentTree.vue
+++ src/views/background/user/DepartmentTree.vue
@@ -6,17 +6,17 @@
       :props="props"
       :load="loadNode"
       node-key="id"
+      :expand-on-click-node="false"
       @node-click="handleNodeClick"
       show-checkbox
       :check-strictly="checkStrickly"
-      @check-change="handleCheckChange"
+      @check="checkNode"
       highlight-current></el-tree>
   </div>
 </template>
 
 <script>
 import { getOrgs } from '@/api/background'
-import { getTree } from '@/utils'
 export default {
   data () {
     return {
@@ -30,12 +30,12 @@ export default {
   },
   methods: {
     handleNodeClick (data) {
+      this.$refs.tree.setCheckedNodes([])
       this.$emit('click', { id: data.id, checked: true })
     },
-    handleCheckChange(data, checked, indeterminate) {
-      if (checked) {
-        this.$emit('click', { id: data.id, checked: false })
-      }
+    checkNode (node, checkStatus) {
+      this.$refs.tree.setCheckedNodes([node])
+      this.$emit('click', { id: node.id, checked: false })
     },
     loadNode (node, resolve) {
       let data = {
@@ -43,7 +43,11 @@ export default {
         pagesize: 10000,
         parentid: node.level === 0 ? 1 : node.data.id
       }
+      if (node.level === 0) {
+        this.navLoading = true
+      }
       getOrgs(data).then(res => {
+        this.navLoading = false
         let tempList = res.list.map(item => {
           return {
             name: item.departmentname,
