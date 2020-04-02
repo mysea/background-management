3d4d97f7b20951d612bfbd4ec40bdd324c6bf0f7
李文 <lwen7@grgbanking.com>
2020-04-02 14:59:03 +0800

    请求错误处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import Lockr from 'lockr'
+import store from '@/store'
+import { toast, confirm } from './toast'
 
 const http = axios.create({
   timeout: 15 * 1000
@@ -10,16 +12,36 @@ http.interceptors.request.use(
     config.headers.Authorization = 'Bearer ' + Lockr.get('Authorization')
     return config
   },
-  error => {
-    return Promise.reject(error)
+  err => {
+    return Promise.reject(err)
   }
 )
 http.interceptors.response.use(
   response => {
     return response.data
   },
-  error => {
-    return Promise.reject(error)
+  err => {
+    if (err && err.response) {
+      const status = err.response.status
+      switch (status) {
+        case 500:
+          toast('error', '服务器500错误')
+          break
+        case 401:
+          confirm('授权已过期，请重新登录', '提示', {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
+            type: 'warning'
+          }).then(() => {
+            store.dispatch('logout')
+            location.reload()
+          }).catch(() => {})
+          break
+        default:
+          break
+      }
+    }
+    return Promise.reject(err)
   }
 )
 
