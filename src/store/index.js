6873fb374a46792adca45b59a3c2c28dc7196d8f
李文 <lwen7@grgbanking.com>
2020-04-08 14:19:48 +0800

    vuex数据持久化

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 import app from './modules/app'
-import permission from './modules/permission'
 import user from './modules/user'
 import getters from './getters'
 
@@ -10,10 +10,10 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
   modules: {
     app,
-    permission,
     user
   },
-  getters
+  getters,
+  plugins: [createPersistedState()]
 })
 
 export default store
